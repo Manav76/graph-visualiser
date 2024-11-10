6f96dde3239688825c5f168c3756cac3108cc90e
ManavJaral <manav.jaral@spyne.ai>
2024-11-10 23:27:17 +0530

    cleanup

--- src/utils/transformEdges.ts
+++ src/utils/transformEdges.ts
@@ -1,21 +1,3 @@
-// transformEdges.ts
-// export const transformEdges = (edges: any[]) => {
-//     return edges.map((edge) => ({
-//       id: `${edge.src_node}-${edge.dst_node}`,
-//       source: edge.src_node,
-//       target: edge.dst_node,
-//       type: 'smoothstep',
-//       animated: true,
-//       label: `${Object.entries(edge.src_to_dst_data_keys)
-//         .map(([srcKey, dstKey]) => `${srcKey} → ${dstKey}`)
-//         .join(', ')}`,
-//       style: { stroke: '#6366f1', strokeWidth: 2 },
-//       labelStyle: { fill: '#ffffff', fontWeight: 'bold' },
-//     }));
-//   };
-  
-
-// import type { Edge } from 'reactflow';
 import type { CustomEdge } from "./graphAlgos";
 
 export const transformEdges = (edges: any[]): CustomEdge[] => {
